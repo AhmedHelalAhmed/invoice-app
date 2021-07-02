2a3fa8ab57b1be67d450c520aee7f745d77213e2
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-07-02 22:24:46 +0200

    Add authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register'=>false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/{page}', [AdminController::class, '__invoke']);
